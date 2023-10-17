a31fea18d9ed7760acc98bb553783591f9d1347f
Студент <@DESKTOP-QSVVOEH>
2023-10-17 14:27:19 +0300

    Добавьте файлы проекта.

---
+++ Kadrov Udin/WinFolder/PresArea.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Kadrov_Udin.WinFolder
+{
+    /// <summary>
+    /// Логика взаимодействия для PresArea.xaml
+    /// </summary>
+    public partial class PresArea : Window
+    {
+        public PresArea()
+        {
+            InitializeComponent();
+        }
+
+        private void leaveAcc_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+    }
+}
---
+++ Kadrov Udin/WinFolder/WinAuthorization.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Kadrov_Udin.WinFolder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kadrov_Udin
+{
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Entrance_Click(object sender, RoutedEventArgs e)
+        {
+            PresArea presArea = new PresArea();
+            presArea.Show();
+            Close();
+        }
+
+        private void Out_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Reg_Click(object sender, RoutedEventArgs e)
+        {
+            WinReg winReg = new WinReg();
+            winReg.Show();
+            Close();
+        }
+    }
+}
